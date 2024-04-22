2b5017877ca517aa28befdbe420dd3bd7d0de09e
Nyamadi Atsu <devbrainy98@gmail.com>
2024-04-22 04:23:11 +0100

    fixed null error on avatar image

--- frontend/src/components/UserAvatar.tsx
+++ frontend/src/components/UserAvatar.tsx
@@ -7,7 +7,8 @@ const UserAvatar = () => {
   const { user } = useAuth();
   const defaultUrl =
     "https://res.cloudinary.com/cloudinary/image/upload/v1667394529/avatars/avatars/default.png";
-  const userProfiePicture = user ? user.profilePicture : defaultUrl;
+  const userProfiePicture =
+    user && user?.profilePicture ? user.profilePicture : defaultUrl;
   const initialsArray = user ? user?.name.split(" ")! : null;
   const fallBackName = initialsArray
     ? `${initialsArray[0].charAt(0)}${initialsArray[1].charAt(0)}`
