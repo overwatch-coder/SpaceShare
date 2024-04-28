f42f047710ce1e7039dcee8d3a6e41842f4e4dfa
Nyamadi Atsu <devbrainy98@gmail.com>
2024-04-28 01:31:29 +0100

    fixed user's name array length error

--- frontend/src/components/UserAvatar.tsx
+++ frontend/src/components/UserAvatar.tsx
@@ -9,7 +9,11 @@ const UserAvatar = ({ nameClass }: { nameClass?: string }) => {
     "https://res.cloudinary.com/cloudinary/image/upload/v1667394529/avatars/avatars/default.png";
   const userProfiePicture =
     user && user.profilePicture ? user.profilePicture : defaultUrl;
-  const initialsArray = user ? user.name.split(" ") : ["Guest", "User"];
+  const initialsArray = user
+    ? user.name.split(" ").length > 1
+      ? user.name.split(" ")
+      : user.name.split("")
+    : ["Guest", "User"];
   const fallBackName = initialsArray
     ? `${initialsArray[0]?.charAt(0)}${initialsArray[1]?.charAt(0)}`
     : "SS";
